15629cd4fae4a7874889aae7d5c0815cc0aa9257
Nathan HOCHSTRASSER <nhochstr@e1r11p13.42.fr>
2019-11-28 00:33:49 +0100

    protect substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nhochstr <nhochstr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 09:49:20 by nhochstr          #+#    #+#             */
-/*   Updated: 2019/11/27 20:53:23 by nhochstr         ###   ########.fr       */
+/*   Updated: 2019/11/28 00:30:31 by nhochstr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	j;
 
 	i = 0;
-	slen = ft_strlen(s);
 	j = len + 1;
 	if (!s)
 		return (NULL);
+	slen = ft_strlen(s);
 	start = (start > slen) ? slen : start;
 	len = (start + len > slen) ? slen - start : len;
 	if (!j || !(ptr = (char *)malloc(sizeof(char) * (len + 1))))
