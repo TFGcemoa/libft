313563b673f18c0492e1c42015a853367449ecdb
Nathan HOCHSTRASSER <nhochstr@e1r11p13.42.fr>
2019-11-28 00:15:26 +0100

    Fix substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nhochstr <nhochstr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 09:49:20 by nhochstr          #+#    #+#             */
-/*   Updated: 2019/11/27 18:51:22 by nhochstr         ###   ########.fr       */
+/*   Updated: 2019/11/27 20:53:23 by nhochstr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
 	size_t	i;
+	size_t	slen;
+	size_t	j;
 
 	i = 0;
+	slen = ft_strlen(s);
+	j = len + 1;
 	if (!s)
 		return (NULL);
-	start = (start > ft_strlen(s)) ? ft_strlen(s) : start;
-	len = (start + len > ft_strlen(s)) ? ft_strlen(s) - start : len;
-	if (!(ptr = (char *)malloc(sizeof(char) * (len + 1))))
+	start = (start > slen) ? slen : start;
+	len = (start + len > slen) ? slen - start : len;
+	if (!j || !(ptr = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-	while (i < len)
+	while (i < len && s[i + start])
 	{
 		ptr[i] = s[i + start];
 		i++;
